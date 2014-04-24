2c31f3cd90bd08f4f966ef810d9400cee07fe082
Gary Edgcombe <garyedgcombe@garys-mbp.lan>
2014-04-24 12:22:57 +0100

    tableViews added

--- TableViewController controller challenge.h
+++ TableViewController controller challenge.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TableViewController controller challenge.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Gary Edgcombe on 24/04/2014.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface TableViewController_controller_challenge : UITableViewController
 
 @end
